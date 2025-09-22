831d9fc8dc9f9f10da116040ca2603f1114084a9
danxguerrero <danxguerrero@yahoo.com>
2025-09-21 21:26:56 -0400

    Add User model and establish relationships with Standup and Journal models in models.py

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime, timezone
 from app import db
 
 # TODO: #4 Define User model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
+    standups = db.relationship('Standup', backref='user', lazy=True)
+    journals = db.relationship('Journal', backref='user', lazy=True)
 
 class Standup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +22,9 @@ class Standup(db.Model):
     public = db.Column(db.Boolean, default=False, nullable=False)
 
 # TODO: #5 Add Standup-User relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    __table_args__ = (db.UniqueConstraint('date', 'user_id', name='uix_user_date'))
 
 class Journal(db.Model):
     id = db.Column(db.Integet, primary_key=True)
@@ -25,3 +35,4 @@ class Journal(db.Model):
     public = db.Column(db.Boolean, default=False, nullable=False)
 
 # TODO: Add Journal-User relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
